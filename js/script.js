13a70d5da1ca83c086b0a6152c62b15debbc125f
gusandrini <gustavosandrini92@gmail.com>
2024-08-08 08:59:57 -0300

    Declarando uma lista de carros

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 //Declarando uma lista de carros
 carros = ["Ferrari", "Porsche", "Lamborghini", "Mercedes", "Audi", "Volkswagen", "Honda"];
 
-const novaLista = carros.map(function () {
-    
+const novaLista = carros.map(function (c) {
+    console.log(c);
 } );
+
